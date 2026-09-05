f90fa1382e5f273eaed2e1e655c4f887e8413235
stevenpelley <stevenpelley@gmail.com>
2026-09-05 13:33:34 +0000

    nvim-trial: add gitsigns hunk keymaps (gitsigns has no defaults)

--- config/nvim-trial/init.lua
+++ config/nvim-trial/init.lua
@@ -91,11 +91,27 @@ require("lazy").setup({
       { "<leader>fC", function() require("telescope.builtin").lsp_outgoing_calls() end, desc = "Outgoing calls" },
       { "<leader>fd", function() require("telescope.builtin").diagnostics() end, desc = "Diagnostics" },
     },
-    opts = {},
   },
-
   -- Git: inline hunk markers + commit/history diff review
-  { "lewis6991/gitsigns.nvim", opts = {} },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+        local map = function(lhs, rhs, desc)
+          vim.keymap.set("n", lhs, rhs, { buffer = bufnr, desc = desc })
+        end
+        map("]h", function() gs.nav_hunk("next") end, "Next hunk")
+        map("[h", function() gs.nav_hunk("prev") end, "Prev hunk")
+        map("<leader>hp", gs.preview_hunk, "Preview hunk")
+        map("<leader>hb", gs.blame_line, "Blame line")
+        map("<leader>hs", gs.stage_hunk, "Stage hunk")
+        map("<leader>hr", gs.reset_hunk, "Reset hunk")
+        map("<leader>hd", gs.diffthis, "Diff file vs index")
+        map("<leader>hD", function() gs.diffthis("~") end, "Diff file vs HEAD~")
+      end,
+    },
+  },
   { "sindrets/diffview.nvim", cmd = { "DiffviewOpen", "DiffviewFileHistory" } },
 }, {
   -- everything lives under the nvim-trial data dir; nuke it to reset
